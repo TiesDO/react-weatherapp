bd1c684bb66488eedd288af882baf94162efa471
TiesDO <denouden.ties@gmail.com>
2023-01-31 20:16:41 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { WeatherListItem } from './components/WeatherListItem';
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Easy react app template!
+            </h1>
+
+            <WeatherListItem />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/WeatherListItem.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function WeatherListItem() {
+    return <h1>I'm a weather list item :D</h1>;
+}
