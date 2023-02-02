6546e92c1c55fcd040fa56a098bfb164628b541b
TiesDO <denouden.ties@gmail.com>
2023-02-02 16:51:30 +0100

    implement reducer actions
    - Reducer
      - set_loading
      - update lat & lon
      - update error
      - update data
    - SelectCity
      - call api
      - set states with reducer

--- src/components/SelectCityForm.jsx
+++ src/components/SelectCityForm.jsx
@@ -1,32 +1,46 @@
-import { useCurrentWeather } from "../context/CurrentWeatherContext";
+import { useCurrentWeather, useCurrentWeatherDispatch } from "../context/CurrentWeatherContext";
 import React, { useState } from 'react';
 import fetchCurrentWeather from "../datafetch/fetchCurrentWeather";
 
 export default function SelectCityForm() {
     const currentWeather = useCurrentWeather();
+    const currentWeatherDispatch = useCurrentWeatherDispatch();
 
     const [city, setCity] = useState('some value');
-    const [lat, setLat] = useState(currentWeather.lat ?? '');
-    const [lon, setLon] = useState(currentWeather.lon ?? '');
+
+    const handleLatChange = (e) => {
+        currentWeatherDispatch({ type: 'update_lat', payload: e.target.value });
+    }
+
+    const handleLonChange = (e) => {
+        currentWeatherDispatch({ type: 'update_lon', payload: e.target.value });
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log(`input was [lat:'${lat}', lon:'${lon}', city:'${city}']`);
+        currentWeatherDispatch({ type: 'set_loading', payload: true});
 
-        // fetch the current weather for these coordinates
-        fetchCurrentWeather(lat, lon)
-            .then((data) => {
-                console.log(data);
+        // make the API call and update the context accordingly
+        fetchCurrentWeather(currentWeather.locationLat, currentWeather.locationLon)
+            .then((data) => { 
+                currentWeatherDispatch({ type: 'update_currentWeather', payload: data }) 
+                currentWeatherDispatch({ type: 'update_error', payload: null});
             })
-            .catch((error) => {
-                console.error(error);
+            .catch((error) => { 
+                // I don't reset the old data so it can still be displayed (as outdated)
+                currentWeatherDispatch({ type: 'update_error', payload: error})
             })
+            .finally(() => {
+                // unset loading, regardless of error state
+                currentWeatherDispatch({ type: 'set_loading', payload: false });
+            });
     }
 
     return <form onSubmit={handleSubmit}>
-        <input type='text' value={lat} onChange={(e) => setLat(e.target.value)} />    
-        <input type='text' value={lon} onChange={(e) => setLon(e.target.value)} />    
+        <input type='text' value={currentWeather.locationLat} onChange={handleLatChange} />    
+        <input type='text' value={currentWeather.locationLon} onChange={handleLonChange} />    
+
         <input type='text' value={city} onChange={(e) => setCity(e.target.value)} />    
         <input type='submit' value='change location' />    
     </form>
--- src/reducers/CurrentWeatherReducer.jsx
+++ src/reducers/CurrentWeatherReducer.jsx
@@ -1,7 +1,31 @@
 export default function CurrentWeatherReducer(state, action) {
     switch(action.type)
     {
-        
+        case 'update_currentWeather':
+            return {
+                ...state,
+                requestData: action.payload
+            }
+        case 'update_error':
+            return {
+                ...state,
+                requestError: action.payload
+            }
+        case 'set_loading':
+            return {
+                ...state,
+                requestLoading: action.payload
+            }
+        case 'update_lat':
+            return {
+                ...state,
+                locationLat: action.payload
+            }
+        case 'update_lon':
+            return {
+                ...state,
+                locationLon: action.payload
+            }
     }
 
     throw new Error('Unknown action: ' + action.type);
