3fa8a3374de02ad461af353e0c5d222b419cd47b
TiesDO <denouden.ties@gmail.com>
2023-02-05 20:54:48 +0100

    add weather icon component

---
+++ src/components/CurrentWeatherDisplay/WeatherCodeIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// TODO: implement more weathercode related icons
+import {
+    WiDaySunny
+} from 'weather-icons-react'
+
+export default function WeatherCodeIcon({weatherCode, icon}) {
+    return 
+}
