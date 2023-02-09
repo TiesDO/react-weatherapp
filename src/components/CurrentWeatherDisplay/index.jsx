ded7f11ec8690089de026b13c30d84cb88f5453c
Ties den Ouden <tiesdenouden@pop-os.localdomain>
2023-02-09 20:44:06 +0100

    add unstyled display of dummy weather data

--- src/components/CurrentWeatherDisplay/index.jsx
+++ src/components/CurrentWeatherDisplay/index.jsx
@@ -3,10 +3,35 @@ import {useCurrentWeather} from '../../context/CurrentWeatherContext';
 import { WiDayCloudy } from "weather-icons-react";
 
 export default function CurrentWeatherDisplay() {
-    // consume the context
     const currentWeather = useCurrentWeather();
 
-    return <div>
-        {currentWeather.requestData && <><WiDayCloudy size={25} /> <span>{currentWeather.requestData.current_weather.temperature}</span></>}
-    </div>
+    return ( 
+        <div className="current-weather-container">
+            <div className="weather-code">
+                <WiDayCloudy size={25} />
+                <h2>8.2</h2>
+            </div>
+            <ul className="data-pane">
+                <li>
+                    <span>windspeed:</span>
+                    <span>12km/h</span>
+                </li>
+                
+                <li>
+                    <span>winddirection:</span>
+                    <span>NE</span>
+                </li>
+
+                <li>
+                    <span>elevation:</span>
+                    <span>12m</span>
+                </li>
+
+                <li>
+                    <span>time:</span>
+                    <span>15:00</span>
+                </li>
+            </ul>
+        </div>
+    )
 }
---
+++ src/components/FutureWeatherDisplay/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function FutureWeatherDisplay() {
+    return (<div>
+        this whill display the future weather.
+    </div>)
+}
+
